012f859158b572d7dcef24b3df48d350d95f207f
Chris <16949750+GangsterGeek89@users.noreply.github.com>
2021-02-05 22:08:09 +0000

    Initial Setup
    Added Required Piranha Packages
    Initial Startup.cs Configuration
    
    At this point you can start and run the application and should be presented with a This localhost page can’t be found error
    
    If you now navigate to /manager
    
    example: https://localhost:44367/manager
    
    And you should be presented with the managers login page  which you can now login with admin/password

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Piranha;
+using Piranha.AspNetCore.Identity.SQLite;
+using Piranha.AttributeBuilder;
+using Piranha.Data.EF.SQLite;
+using Piranha.Local;
 
 namespace MyPiranhaExamples
 {
@@ -16,24 +22,55 @@ namespace MyPiranhaExamples
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddPiranha(options =>
+            {
+                options.AddRazorRuntimeCompilation = true;
+
+                options.UseFileStorage(naming: FileStorageNaming.UniqueFolderNames);
+                options.UseImageSharp();
+                options.UseManager();
+                options.UseTinyMCE();
+                options.UseMemoryCache();
+
+                options.UseEF<SQLiteDb>(db =>
+                    db.UseSqlite("Filename=./gangstergeek.razorweb.db"));
+                options.UseIdentityWithSeed<IdentitySQLiteDb>(db =>
+                    db.UseSqlite("Filename=./gangstergeek.razorweb.db"));
+
+                options.UseSecurity(o =>
+                {
+                    o.UsePermission("Subscriber");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApi api)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            App.Init(api);
 
-            app.UseEndpoints(endpoints =>
+            // Configure cache level
+            App.CacheLevel = Piranha.Cache.CacheLevel.Full;
+
+            // Build content types
+            new ContentTypeBuilder(api)
+                .AddAssembly(typeof(Startup).Assembly)
+                .Build()
+                .DeleteOrphans();
+
+            // Configure editor
+            Piranha.Manager.Editor.EditorConfig.FromFile("editorconfig.json");
+
+            app.UsePiranha(options =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                options.UseManager();
+                options.UseTinyMCE();
+                options.UseIdentity();
             });
         }
     }
