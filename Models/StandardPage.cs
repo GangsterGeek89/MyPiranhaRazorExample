8964627a9f6ea0fc7da69b9d460496e89a4e7b4c
Chris <16949750+GangsterGeek89@users.noreply.github.com>
2021-02-05 22:34:58 +0000

    Created First Page Type & Layout Files
    Created the _ViewStart, _ViewImports & _Layout Files
    
    Created The StandardPage Class & Page.cshtml Files
    
    At this point you can start up the application again which you should be presented with the same error as earlier This localhost page can’t be found error
    
    Now again login to the manager and now in the add page section you should see the option for your Standard Page. Click this and then at the very top give the page a title and hit publish. If all worked you should see a page saved message :)
    
    Now navigate back to your default URL
    Example: https://localhost:44367/
    
    You should see our standard page show instead of the error we was getting earlier. Its all fun and games from here

---
+++ Models/StandardPage.cs
@@ -0,0 +1,14 @@
+﻿using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace MyPiranhaExamples.Models
+{
+    /// <summary>
+    /// Basic page with main content in markdown.
+    /// </summary>
+    [PageType(Title = "Standard Page")]
+    [PageTypeRoute(Title = "Default", Route = "/page")]
+    public class StandardPage : Page<StandardPage>
+    {
+    }
+}
